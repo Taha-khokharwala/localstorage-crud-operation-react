c22485c66b6ef426df7c0f28cef74825248f5154
Devansh Mota <devanshmota@gmail.com>
2024-01-15 16:28:08 +0530

    first commit

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,49 +1,51 @@
-import React, { Fragment , useState } from 'react';
+import React, { Fragment, useState } from 'react';
 
 function Login() {
   const [emailValue, setEmailValue] = useState('');
   const [passwordValue, setPasswordValue] = useState('');
   const [users, setUsers] = useState([]);
-
+  const [id, setId] = useState(1);
 
   const insertData = (event) => {
     event.preventDefault();
-    setUsers([...users, { email: emailValue, password: passwordValue }]);
-    setEmailValue('')
-    setPasswordValue('')
+    const userId = id;
+    setUsers([...users, { id: userId, email: emailValue, password: passwordValue }]);
+    setEmailValue('');
+    setPasswordValue('');
+    setId(id + 1);
 
-    localStorage.setItem(emailValue , passwordValue)
+    localStorage.setItem(userId.toString(), JSON.stringify({email: emailValue, password: passwordValue }));
   };
 
-  
-
   const updateData = (index) => {
-    const { email, password } = users[index];
-    setEmailValue(email);
-    setPasswordValue(password);
-    const newData = [...users];
-    newData.splice(index, 1);
-    setUsers(newData);
+  const { email, password, id } = users[index];
 
-    localStorage.store = email
-    localStorage.store = password
-    localStorage.removeItem(email);
+  localStorage.setItem(id.toString(), JSON.stringify({ email, password }));
+
+  const newData = [...users];
+  newData.splice(index, 1);
+  setUsers(newData);
+
+  setEmailValue(email);
+  setPasswordValue(password);
+  setId(id);
 };
 
   const deleteData = (index) => {
-    const newData = [...users]
-    newData.splice(index ,1)
+    const { id } = users[index];
+    const newData = [...users];
+    newData.splice(index, 1);
     setUsers(newData);
-    localStorage.clear();
-  }
-
+    setId(id)
+    localStorage.removeItem(id.toString());
+  };
 
   return (
     <form onSubmit={insertData}>
       <Fragment>
         <label>Email: </label>
         <input
-          type='text'
+          type='email'
           value={emailValue}
           pattern='[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$'
           required
@@ -59,17 +61,20 @@ function Login() {
         <table border={1}>
           <tbody>
             <tr>
-              <td>email</td>
+              <td>ID</td>
+              <td>Email</td>
               <td>Password</td>
               <td>Action</td>
             </tr>
             {users.map((user, index) => (
               <tr key={index}>
+                <td>{user.id}</td>
                 <td>{user.email}</td>
                 <td>{user.password}</td>
                 <td>
-                <button onClick={() => updateData(index)}>update</button>
-                <button onClick={() => deleteData(index)}>Delete</button></td>
+                  <button onClick={() => updateData(index)}>Update</button>
+                  <button onClick={() => deleteData(index)}>Delete</button>
+                </td>
               </tr>
             ))}
           </tbody>
